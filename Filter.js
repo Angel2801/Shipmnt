90e0263a0bd9d314e83b1f1dfcf81bf078922282
Angel2801 <21bce017@nirmauni.ac.in>
2024-08-05 10:15:19 +0530

    Range function added

--- Filter.js
+++ Filter.js
@@ -1,1 +1,35 @@
-import React from 'React';
+import React, { useState } from 'React';
+
+const Filter=({column,onFilterChange})=>{
+    const [filterType,setFilterType]=useState('');
+    const [filterValue,setFilterValue]=useState('');
+    const handleFilterChange=()=>{
+        onFilterChange({
+            column:column.name,
+            type:filterType,
+            value:filterValue
+        });
+    };
+}
+const FilterOptions=()=>{
+    switch(column.type){
+        case 'Integer':
+            return(
+                <>
+                <select value={filterType} onChange={(e)=>setFilterType(e.target.value)}>
+                    <option value="">Select Filter</option>
+                    <option value="equals">Equals</option>
+                    <option value="lessThan">Less than</option>
+                    <option value="lessThanOrEqual">Less than or equal</option>
+                    <option value="greaterThan">Greater than</option>
+                    <option value="greaterThanOrEqual">Greater than or equal</option>
+                    <option value="range">Range</option>
+                    <option value="notEqual">Not equal</option>
+                </select>
+                {filterType==="range"?(
+                    
+                )}
+                </>
+            )
+    }
+}
